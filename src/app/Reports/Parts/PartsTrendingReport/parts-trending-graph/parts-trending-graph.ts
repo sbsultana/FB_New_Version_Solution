2e88a45361df5cdf8ba91cea87e3a33fd10db91e
sbsultana <sbsultana@spyhre.com>
2026-03-20 13:07:30 +0530

    Parts Trending Report

---
+++ src/app/Reports/Parts/PartsTrendingReport/parts-trending-graph/parts-trending-graph.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parts-trending-graph',
+  imports: [],
+  templateUrl: './parts-trending-graph.html',
+  styleUrl: './parts-trending-graph.scss',
+})
+export class PartsTrendingGraph {
+
+}
